48023bb4152046f658ecaefac4b0624ca2930f22
Rusu83 <35503591+Rusu83@users.noreply.github.com>
2020-11-18 15:14:43 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,10 @@ function setup() {
 function draw() {
   background(0,40,40);
   ellipse(x,height/2,50,50);
-  v=f/(a+b);
-  x+=v;
   if (keyIsDown(DOWN_ARROW))
     f=15;
   else
     f=0;
+  v=f/(a+b);
+  x+=20*v;
 }
